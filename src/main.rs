511a3b459bbd11ef14c40dcd656fccbc0049987c
ARez <dark.gaming@fantasymail.de>
2023-03-19 13:58:07 +0100

    Add documentation for init, simplify init usage

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{path::{PathBuf}, str::FromStr};
+use std::{path::{PathBuf, self}, str::FromStr};
 
 use anyhow::{Result, Error, Context, Ok};
 use clap::{Args, Parser, Subcommand, builder};
@@ -9,6 +9,7 @@ use gdsetup::codegen;
 const GODOT_DIR: &'static str = "godot";
 const SRC_DIR: &'static str = "src";
 const CLASS_NAME: &'static str = "example";
+const DEFAULT_PROJECTNAME: String = String::new();
 
 
 #[cfg_attr(name, String)]
@@ -16,13 +17,17 @@ const CLASS_NAME: &'static str = "example";
 #[command(about = "Sets up a Godot C++ project.", long_about = None)]
 #[command(author, version, about, long_about = None)]
 struct GDSetup {
+    /// Optional: Shorthand version for 'gdsetup init [NAME]'
+    name: Option<String>,
+
     #[command(subcommand)]
     command: Option<Commands>,
+
 
     #[arg(
         long,
         short,
-        default_value_t = String::from("info"),
+        default_value_t = String::from("trace"),
         value_parser = clap::builder::PossibleValuesParser::new(["error", "warn", "info", "debug", "trace"]),
     )]
     log_level: String,
@@ -34,18 +39,24 @@ struct NameArgs {
 }
 
 #[derive(Args, Debug)]
-struct PathArgs {
+struct InitArgs {
+    /// Optional: A shorthand version for [--path | -p]
     path: Option<PathBuf>,
+
+    /// Path to an existing Godot project folder
+    #[arg(long = "path", short = 'p', value_name = "SPECIFIC PATH")]
+    path2: Option<PathBuf>,
 }
+
 
 #[derive(Subcommand, Debug)]
 enum Commands {
     /// IF project.godot: Move everything into godot/    ELSE: Creates the folders and example dummy files
-    Init(PathArgs),
-    /// Creates another Extension
-    Add(NameArgs),
+    Init(InitArgs),
     /// Renames all occurences of the extension name (or the files)
     Rename(NameArgs),
+    /// Creates another GDExtension module
+    Add(NameArgs),
 }
 
 
@@ -75,48 +86,67 @@ fn main() -> Result<(), Error> {
     std::env::set_var("RUST_LOG", format!("={}", level));
     env_logger::init_from_env(env_logger::Env::new());
 
-    if let std::result::Result::Ok(_) = std::fs::remove_dir_all(format!(".\\test\\testproject\\{}", GODOT_DIR).as_str()) {
-
-    };
-    if let std::result::Result::Ok(_) = std::fs::remove_dir_all(format!(".\\test\\testproject\\{}", SRC_DIR).as_str()) {
-
-    };
-    //return Ok(());
-    
-    std::fs::copy(".\\test\\project.godot", ".\\test\\testproject\\project.godot")?;
-    std::fs::copy(".\\test\\test.txt", ".\\test\\testproject\\test.txt")?;
-    //return Ok(());
-
     debug!("{args:?}");
     if let Some(command) = args.command {
         match command {
-            Commands::Init(pathargs) => init(pathargs),
-            Commands::Add(nameargs) => add_extension(nameargs),
+            Commands::Init(pathargs) => init(pathargs, args.name),
             Commands::Rename(nameargs) => rename(nameargs),
+            Commands::Add(nameargs) => add_extension(nameargs),
         }
     } else {
-        init(PathArgs { path: None })
+        init(InitArgs { path: None, path2: None}, args.name)
     }
 
 }
 
 // return Err(Error::msg(format!("Directory `{}` is not empty. If you want to continue add the -f (--force) option", pathstr)));
-fn init(pathargs: PathArgs) -> Result<(), Error> {
-    let path = match pathargs.path {
-        Some(path) => path,
-        None => PathBuf::from(std::env::current_dir().unwrap_or_default())
+fn init(pathargs: InitArgs, projectname: Option<String>) -> Result<(), Error> {
+    // Decide whether to use the shorthand path version if provided
+    let p = {
+        if pathargs.path.is_some() {
+            pathargs.path
+        } else if pathargs.path2.is_some() {
+            pathargs.path2
+        } else {
+            None
+        }
+    };
+
+    // One of them must be given
+    // TODO: Maybe allow this but with a confirmation as it could change the current folder
+    if p.is_none() && projectname.is_none() {
+        return Err(Error::msg("Missing either a --path (-p) to an exisiting folder or a name for a new one."))
+    };
+
+    let current_dir = std::env::current_dir().unwrap_or_default();
+
+    // Create a new project folder
+    let path = if let Some(name) = projectname {
+        let dir = current_dir.join(name);
+        let _d = dir.clone();
+        let dirname = _d.display();
+        info!("Creating new project folder '{}'", dirname);
+        std::fs::create_dir(dir.clone()).with_context(|| format!("Tried to create a new folder '{}'", dirname))?;
+        dir
+    } else {
+        match p {
+            Some(path) => path,
+            None => current_dir,
+        }
     };
     let pathstr = path.to_str().unwrap();
     let read = std::fs::read_dir(path.clone())?;
     let read = read.into_iter().flatten().collect::<Vec<std::fs::DirEntry>>();
 
+    // Create the 'godot' folder
     let godot_folder = path.clone().join(GODOT_DIR);
     let godot_folder_str = godot_folder.clone().display().to_string();
     std::fs::create_dir(godot_folder.clone()).with_context(|| format!("Failed to create directory '{}'", godot_folder_str))?;
 
     let project_exists = read.iter().find(|f| f.file_name() == "project.godot").is_some();
+    // If there's a project.godot: Move everything to 'godot/'
     if project_exists {
-        info!("Moving everything inside '{}' into '{}'", pathstr, godot_folder_str);
+        info!("Found project.godot. Moving everything inside '{}' into '{}'", pathstr, godot_folder_str);
         for file in read.iter() {
             let orig = path.join(file.file_name());
             let orig_disp = orig.display().to_string();
@@ -125,34 +155,35 @@ fn init(pathargs: PathArgs) -> Result<(), Error> {
             info!("Copying `{}` into `{}` ...", orig_disp, dest_disp);
             std::fs::copy(orig, dest).with_context(|| format!("Tried copying {} into {}", orig_disp, dest_disp))?;
         };
-        // Godot project folder has been created
+        // Godot project folder has been created, remove old files
         for file in read.iter() {
             let filename = path.clone().join(file.file_name());
-            info!("Removing '{:?}' from '{}' ...", file.file_name().to_str().unwrap(), pathstr);
+            info!("Removing {:?} from '{}' ...", file.file_name().to_str().unwrap(), pathstr);
             std::fs::remove_file(filename).with_context(|| format!("Failed to remove '{:?}' from {}", file.file_name().to_str().unwrap(), pathstr))?;
         };
     };
     
+    // Create the 'src/' folder
     let src_folder = path.clone().join(SRC_DIR);
     let src_folder_str = src_folder.clone().display().to_string();
     std::fs::create_dir(src_folder.clone()).with_context(|| format!("Failed to create directory '{}'", src_folder_str))?;
 
+    // Create the registration files
     std::fs::write(src_folder.clone().join("register_types.cpp"), codegen::generate_register_cpp(CLASS_NAME))?;
-
     std::fs::write(src_folder.clone().join("register_types.h"), codegen::generate_register_h(CLASS_NAME))?;
-    
+    // Create the class files
     std::fs::write(src_folder.clone().join(format!("{}.cpp", CLASS_NAME)), codegen::generate_class_cpp(CLASS_NAME))?;
-    
     std::fs::write(src_folder.clone().join(format!("{}.h", CLASS_NAME)), codegen::generate_class_h(CLASS_NAME))?;
     Ok(())
 }
 
 
-fn add_extension(nameargs: NameArgs) -> Result<(), Error> {
+
+fn rename(nameargs: NameArgs) -> Result<(), Error> {
     Ok(())
 }
 
 
-fn rename(nameargs: NameArgs) -> Result<(), Error> {
+fn add_extension(nameargs: NameArgs) -> Result<(), Error> {
     Ok(())
 }
